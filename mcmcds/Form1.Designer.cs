be418fd9bda01f8f8c426db73962f017b7e834eb
PawelAdamczuk <p.m.adamczuk@gmail.com>
2016-01-17 18:48:26 +0100

    deleted //dupa

--- mcmcds/Form1.Designer.cs
+++ mcmcds/Form1.Designer.cs
@@ -31,7 +31,6 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Text = "Form1";
-            //dupa
         }
 
         #endregion
