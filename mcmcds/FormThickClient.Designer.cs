adca9da61867dfffcaee55ce5fb94be569becf44
PawelAdamczuk <p.m.adamczuk@gmail.com>
2016-01-20 17:44:52 +0100

    Added a banana icon.

--- mcmcds/FormThickClient.Designer.cs
+++ mcmcds/FormThickClient.Designer.cs
@@ -28,13 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormThickClient));
             this.SuspendLayout();
             // 
             // FormThickClient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(574, 395);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormThickClient";
             this.Text = "FormThickClient";
             this.ResumeLayout(false);
