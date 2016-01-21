15c92651a32c430745e8c2baab2896d5437cce88
PawelAdamczuk <p.m.adamczuk@gmail.com>
2016-01-21 15:34:47 +0100

    FormAddMeal changes

--- mcmcds/FormAddMeal.cs
+++ mcmcds/FormAddMeal.cs
@@ -16,6 +16,7 @@ namespace mcmcds
     {
         private string connectionString;
         private List<string> itemList;
+        private double priceSum;
 
         private bool isValid()
         {
@@ -30,6 +31,7 @@ namespace mcmcds
         {
             this.connectionString = _connectionString;
             itemList = new List<string>();
+            priceSum = 0;
             InitializeComponent();
 
         }
@@ -44,7 +46,7 @@ namespace mcmcds
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                SqlDataAdapter da = new SqlDataAdapter("SELECT name, CAST ( price AS float )/100 AS \"price\", description FROM ITEMS", conn);
+                SqlDataAdapter da = new SqlDataAdapter("SELECT id, name, CAST ( price AS float )/100 AS \"price\", description FROM ITEMS", conn);
                 SqlCommandBuilder cb = new SqlCommandBuilder(da);
 
                 DataSet ds = new DataSet();
@@ -58,19 +60,88 @@ namespace mcmcds
 
         private void button_AddItem_Click(object sender, EventArgs e)
         {
-             listView_mealItems.Items.Add(dataGridView_items.SelectedRows[0].Cells["name"].Value.ToString());
+            listView_mealItems.Items.Add(dataGridView_items.SelectedRows[0].Cells["name"].Value.ToString());
+            //MessageBox.Show(dataGridView_items.SelectedRows[0].Cells["price"].ToString());
+            priceSum += ((double)Utilities.PriceField(dataGridView_items.SelectedRows[0].Cells["price"].Value.ToString()))/100;
+            if (priceSum < 0.1)
+                priceSum = 0;
+            textBox_calculatedPrice.Text = priceSum.ToString();
         }
 
         private void button_remove_Click(object sender, EventArgs e)
         {
             try
             {
-                listView_mealItems.SelectedItems[0].Remove();
+                for(int i = 0; i < dataGridView_items.RowCount; ++i){
+                    if(dataGridView_items.Rows[i].Cells["name"].Value.ToString().Equals(listView_mealItems.SelectedItems[0].Text)) {
+                        priceSum -= ((double)Utilities.PriceField(dataGridView_items.Rows[i].Cells["price"].Value.ToString())) / 100;
+                    }
                 }
+                if (priceSum < 0.1)
+                    priceSum = 0;
+                textBox_calculatedPrice.Text = priceSum.ToString();
+                listView_mealItems.SelectedItems[0].Remove();
+
+                
+            }
             catch (Exception exception)
             {
-                MessageBox.Show("Idiot...");
+                MessageBox.Show("Baka...");
                 MessageBox.Show("There's nothing to delete.");
+            }
+        }
+
+        private void button_addMeal_Click(object sender, EventArgs e)
+        {
+            if (!isValid())
+            {
+                MessageBox.Show("Input contains illegal characters.");
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+
+                try
+                {
+                    int priceInt = Utilities.PriceField(textBox_mealPrice.Text);
+                    conn.Open();
+                    string command = "INSERT into MEALS (name, discount) " +
+                                     $"VALUES ('{textBox_mealName.Text}', '{(int)(priceSum*100)-priceInt}') SET @ID = SCOPE_IDENTITY();";
+                    SqlCommand insertQuery = new SqlCommand(command, conn);
+
+                    insertQuery.Parameters.Add("@ID", SqlDbType.Int, 4).Direction = ParameterDirection.Output;
+                    insertQuery.ExecuteNonQuery();
+
+                    int mealId = (int)insertQuery.Parameters["@ID"].Value;
+
+                    foreach (ListViewItem li in listView_mealItems.Items)
+                    {
+
+                        int itemId = 190;
+                        for (int i = 0; i < dataGridView_items.RowCount; ++i)
+                        {
+                            if (dataGridView_items.Rows[i].Cells["name"].Value.ToString().Equals(li.Text))
+                            {
+                                itemId = (int)dataGridView_items.Rows[i].Cells["id"].Value;
+                            }
+                        }
+
+                        string command2 = "INSERT into MEALS_ITEMS (meal_id, item_id) " +
+                                          $"VALUES ('{mealId}', '{itemId}');";
+                        SqlCommand insertQuery2 = new SqlCommand(command, conn);
+                        insertQuery2.Parameters.Add("@ID", SqlDbType.Int, 4).Direction = ParameterDirection.Output;
+                        insertQuery2.ExecuteNonQuery();
+                    }
+
+                    MessageBox.Show(mealId.ToString());
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show("Error while inserting: " + exception.Message);
+                    return;
+                }
+                MessageBox.Show("Added item.");
             }
         }
     }
